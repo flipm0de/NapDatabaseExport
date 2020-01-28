88c5fed984dfb824b8f26da00c607118194ff077
Valery Dachev <vdachev@david.bg>
2020-01-28 12:33:47 +0200

    Add a first version of the "Export" slide.

---
+++ src/NraDatabaseExport.App/Slides/ExportSlide.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace NraDatabaseExport.App.Slides
+{
+	/// <summary>
+	/// Represents a user control for selecting database tables.
+	/// </summary>
+	public partial class ExportSlide : UserControl
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ExportSlide"/> class.
+		/// </summary>
+		public ExportSlide()
+		{
+			InitializeComponent();
+		}
+	}
+}
